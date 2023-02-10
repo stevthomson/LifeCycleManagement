f3b0206f97c0a802ae07d45deb5195ec4f72295a
stevennetdiverse <steven@netdiverse.com>
2023-02-10 15:59:22 -0700

    save instances

--- app/src/main/java/com/example/lifecyclemanagement/LoggedIn.kt
+++ app/src/main/java/com/example/lifecyclemanagement/LoggedIn.kt
@@ -5,18 +5,38 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoggedIn: AppCompatActivity()  {
+
+    private var firstName: String = ""
+    private var lastName: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
-
-        displayWelcomMessage()
+        if(savedInstanceState != null)
+            loadPrevSession(savedInstanceState)
+        else
+            displayWelcomeMessage()
     }
 
-    private fun displayWelcomMessage() {
-        val firstName = intent.getStringExtra("firstName")
-        val lastName = intent.getStringExtra("lastName")
+    private fun loadPrevSession(savedInstanceState: Bundle) {
+        firstName = savedInstanceState.getString("firstName", "")
+        lastName = savedInstanceState.getString("lastName", "")
+        displayWelcomeMessage(firstName, lastName)
+    }
+
+    private fun displayWelcomeMessage(fName : String="", lName : String = "") {
+        if(fName == "" || lName == "")
+        {
+            firstName = intent.getStringExtra("firstName").toString()
+            lastName = intent.getStringExtra("lastName").toString()
+        }
 
         val message = findViewById<TextView>(R.id.welcome_message) as TextView
         message.text = firstName + " " + lastName +" is logged in!"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("firstName", firstName)
+        outState.putString("lastName", lastName)
     }
 }
