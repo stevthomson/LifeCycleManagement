c3030466c9b475de1505f1d8b537af472fe89d71
stevennetdiverse <steven@netdiverse.com>
2023-02-10 12:46:09 -0700

    image place holder

--- app/src/main/java/com/example/lifecyclemanagement/MainActivity.kt
+++ app/src/main/java/com/example/lifecyclemanagement/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var firstName: String = ""
+    private var middleName: String = ""
+    private var lastName: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
