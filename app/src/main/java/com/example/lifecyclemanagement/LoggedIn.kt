9bc5abb01768d53283149b7834bb43c86724ba2f
stevennetdiverse <steven@netdiverse.com>
2023-02-17 11:31:49 -0700

    profile picture

--- app/src/main/java/com/example/lifecyclemanagement/LoggedIn.kt
+++ app/src/main/java/com/example/lifecyclemanagement/LoggedIn.kt
@@ -1,6 +1,8 @@
 package com.example.lifecyclemanagement
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,6 +10,8 @@ class LoggedIn: AppCompatActivity()  {
 
     private var firstName: String = ""
     private var lastName: String = ""
+    private var picturePath : String = ""
+    private lateinit var image : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
@@ -21,6 +25,7 @@ class LoggedIn: AppCompatActivity()  {
     private fun loadPrevSession(savedInstanceState: Bundle) {
         firstName = savedInstanceState.getString("firstName", "")
         lastName = savedInstanceState.getString("lastName", "")
+        picturePath = savedInstanceState.getString("picturePath", "")
         displayWelcomeMessage(firstName, lastName)
     }
 
@@ -30,10 +35,14 @@ class LoggedIn: AppCompatActivity()  {
         {
             firstName = intent.getStringExtra("firstName").toString()
             lastName = intent.getStringExtra("lastName").toString()
+            picturePath = intent.getStringExtra("picturePath").toString()
         }
 
-        val message = findViewById<TextView>(R.id.welcome_message) as TextView
+        val message = findViewById<TextView>(R.id.welcome_message)
         message.text = firstName + " " + lastName +" is logged in!"
+        println(picturePath)
+        image = findViewById<ImageView>(R.id.img_profileImage)
+        image.setImageBitmap(BitmapFactory.decodeFile(picturePath))
     }
 
 
@@ -41,5 +50,6 @@ class LoggedIn: AppCompatActivity()  {
         super.onSaveInstanceState(outState)
         outState.putString("firstName", firstName)
         outState.putString("lastName", lastName)
+        outState.putString("picturePath", picturePath)
     }
 }
--- app/src/main/java/com/example/lifecyclemanagement/MainActivity.kt
+++ app/src/main/java/com/example/lifecyclemanagement/MainActivity.kt
@@ -1,26 +1,52 @@
 package com.example.lifecyclemanagement
 
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+import java.io.FileOutputStream
+
 
 class MainActivity : AppCompatActivity() {
 
     private var firstName: String = ""
     private var middleName: String = ""
     private var lastName: String = ""
-    private lateinit var profilePic : Bitmap
+    private var picturePath : String = ""
+    private var profilePic : Bitmap? = null
     private lateinit var inputFirstName : EditText
     private lateinit var inputMiddleName : EditText
     private lateinit var inputLastName : EditText
+    private lateinit var image : ImageView
+
+    private var camera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            profilePic =result.data!!.extras!!["data"] as Bitmap?
+            image = findViewById(R.id.img_profileImage) as ImageView
+            image.setImageBitmap(profilePic)
+
+            if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+                picturePath = "/data/user/0/com.example.lifecyclemanagement/app_myPics/myProfilePicture.png"
+                saveImage()
+            } else {
+                Toast.makeText(this, "Cannot save image", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,10 +64,13 @@ class MainActivity : AppCompatActivity() {
             firstName = savedInstanceState.getString("firstName", "")
             middleName = savedInstanceState.getString("middleName", "")
             lastName = savedInstanceState.getString("lastName", "")
+            picturePath = savedInstanceState.getString("picturePath", "")
+            image = findViewById(R.id.img_profileImage) as ImageView
 
             inputFirstName.setText(firstName)
             inputMiddleName.setText(middleName)
             inputLastName.setText(lastName)
+            image.setImageBitmap(BitmapFactory.decodeFile(picturePath))
         }
 
     }
@@ -69,13 +98,35 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun takeProfilePicture() {
-        TODO("Not yet implemented")
+        camera.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+    }
+
+    private fun saveImage() {
+        val cw = ContextWrapper(applicationContext)
+        val directory = cw.getDir("myPics", Context.MODE_PRIVATE)
+        println(directory.absolutePath)
+        directory.mkdir()
+
+        val file = File(directory, picturePath)
+        if(file.exists())
+            file.delete()
+
+        try {
+            val outputStream = FileOutputStream(file)
+            profilePic?.compress(Bitmap.CompressFormat.PNG, 50, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        }
+        catch (e :Exception)
+        {
+            println(e)
+        }
     }
 
 
     private fun logIn(){
-//        if(this::profilePic.isInitialized)
-//        {
+        if(image.drawable != null)
+        {
             // require first and last name, but middle is optional
             firstName = inputFirstName.text.toString()
             middleName = inputMiddleName.text.toString()
@@ -91,15 +142,16 @@ class MainActivity : AppCompatActivity() {
                     val intent :Intent = Intent(this@MainActivity, LoggedIn::class.java)
                     intent.putExtra("firstName", firstName)
                     intent.putExtra("lastName", lastName)
+                    intent.putExtra("picturePath",  picturePath)
                     startActivity(intent)
                 } catch (ex: ActivityNotFoundException) {
                 }
             }
-//        }
-//        else
-//        {
-//            Toast.makeText(applicationContext,"First take a profile picture!",Toast.LENGTH_SHORT).show()
-//        }
+        }
+        else
+        {
+            Toast.makeText(applicationContext,"First take a profile picture!",Toast.LENGTH_SHORT).show()
+        }
     }
 
 
@@ -108,6 +160,6 @@ class MainActivity : AppCompatActivity() {
         outState.putString("firstName", firstName)
         outState.putString("middleName", middleName)
         outState.putString("lastName", lastName)
-        // save profile picture too
+        outState.putString("picturePath", picturePath)
     }
 }
